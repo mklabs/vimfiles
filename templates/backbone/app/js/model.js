1ab70598a3e912ed686c81fd2f8142a357d222b1
Mickael Daniel <daniel.mickael@gmail.com>
2016-05-31 20:59:51 +0200

    Remove templates directory

--- templates/backbone/app/js/model.js
+++
@@ -1,7 +0,0 @@
-// Generate a basic Backbone model
-
-var {{ name }} = Backbone.Model.extend({
-  initialize: function () {
-
-  }
-});
--- templates/backbone/model.js
+++
@@ -1,9 +0,0 @@
-// Generate a basic Backbone model
-//
-// output: ./models/{{ name }}.js
-
-var {{ name }} = Backbone.Model.extend({
-  initialize: function () {
-
-  }
-});

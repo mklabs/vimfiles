c532af50c083c223fd383123f6adec57c13d6e79
Mickael Daniel <daniel.mickael@gmail.com>
2018-10-04 19:32:13 +0200

    oni: update onivim config

--- oni/config.tsx
+++ oni/config.tsx
@@ -1,9 +1,23 @@
-import * as Oni from 'oni-api';
+const activate = oni => {
+  const isMode = mode => {
+    const isMenuOpen = oni.menu.isMenuOpen();
+    const editorMode = oni.editors.activeEditor.mode;
+    return mode === 'menu' ? isMenuOpen : editorMode === mode;
+  };
 
-export const configuration = {
-  //add custom config here, such as
+  const commandMode = () => isMode('cmdline_normal');
+
+  // Panes
+  oni.input.bind('<f8>', 'markdown.togglePreview');
+  oni.input.bind('<f9>', 'sidebar.toggle');
+
+  // Command-line
+  oni.input.bind('<c-v>', 'editor.clipboard.paste', commandMode);
+};
+
+module.exports = {
+  activate,
   'ui.colorscheme': 'nord',
-  //"oni.bookmarks": ["~/Documents"],
   'oni.loadInitVim': true,
   'oni.useDefaultConfig': false,
   'editor.fontSize': '14px',
@@ -13,6 +27,11 @@ export const configuration = {
   'ui.fontSmoothing': 'auto',
 
   'snippets.enabled': false,
+
+  'sidebar.plugins.enabled': true,
+
+  // Oni's clipboard-manager adds bugs and yields no benefits
+  'editor.clipboard.enabled': false,
 
   'oni.plugins.prettier': {
     settings: {
@@ -32,5 +51,10 @@ export const configuration = {
 
     // TODO: Set this to true to enable the plugin
     enabled: true
-  }
+  },
+
+  // Experimental features
+  'editor.quickInfo.delay': 100,
+  'sidebar.marks.enabled': true,
+  'experimental.markdownPreview.enabled': true
 };

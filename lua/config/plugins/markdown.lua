3ecfe7c8101072a15493cdbe19987188cbb8a28f
Mickael Daniel <daniel.mickael@gmail.com>
2025-01-08 18:57:04 +0100

    Adding in markdown plugins
    markdown render, but not enabled
    
    markdown preview

---
+++ lua/config/plugins/markdown.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    enabled = false,
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      render_modes = { 'n', 'c', 't' }
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+};

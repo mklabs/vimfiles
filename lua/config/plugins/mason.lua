cc12917df21014106f1e5ea1d18151288c80bb50
Mickael Daniel <daniel.mickael@gmail.com>
2025-07-16 18:41:31 +0200

    Add Mason and Sleuth plugin configurations
    Introduced configuration files for mason.nvim and vim-sleuth plugins under lua/config/plugins. This adds Mason for managing external lsp tools and Sleuth for automatic indentation detection.

---
+++ lua/config/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
+  }
+};
